012d70aab6613025ac31f49c97232ca209b5f92e
Dohn Joe <johnphtdo@gmail.com>
2020-02-01 18:33:58 -0600

    Added linkedin and github links to the home page and a contact

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ function Home() {
       <div className="content-wrap">
         <div className= "row">
           <div className = "col-12">
-        <h1 className = "title">About Me</h1>
+        <h2 className = "title">About John Do</h2>
         </div>
         </div>
         <div className = "row">
@@ -35,7 +35,8 @@ function Home() {
 
         <p className = "bio">
           My portfolio highlights all the progress that I have made with this
-          program in order to be a valuable asset for any employer.
+          program in order to be a valuable asset for any employer. If you want to learn more about me or my projects, you can follow me on <a className="profile-link" target = "_blank" rel="noopener noreferrer" href = "https://www.linkedin.com/in/johnphtdo/" alt = "LinkedIn Profile">LinkedIn</a> and on <a className="profile-link" target = "_blank" rel="noopener noreferrer" href = "https://www.github.com/johnphtdo/" alt = "Github Profile">Github</a>.
+          You may also reach out to me at johnphtdo@gmail.com.
         </p>
         </div>
         </div>
